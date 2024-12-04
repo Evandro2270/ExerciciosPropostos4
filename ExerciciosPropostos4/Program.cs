1598b166a181f2e0d7c0deab304ea88fcf497275
Evandro2270 <evandro535@gmail.com>
2024-12-04 12:14:51 -0300

    Adicionar arquivos de projeto.

---
+++ ExerciciosPropostos4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.Serialization;
+namespace Programa
+{
+    using System;
+
+    namespace URI
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                CultureInfo CI = CultureInfo.InvariantCulture;
+
+                int number = int.Parse(Console.ReadLine());
+                int hora = int.Parse(Console.ReadLine());
+                double valorHora = double.Parse(Console.ReadLine(), CI);
+
+                double salario = hora * valorHora;
+
+                Console.WriteLine("Number = " + number);
+                Console.WriteLine("Salary = " + salario.ToString("F2", CI));
+
+            }
+        }
+    }
+}
